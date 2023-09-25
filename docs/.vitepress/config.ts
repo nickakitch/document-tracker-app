645e7d3ff57b44df8951ed26b857056cf3b678b5
James Mooring <james@lioneagle.solutions>
2023-09-26 07:41:23 +1000

    change docs root

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/docs/",
   title: "Document Tracker App",
   description: "A document tracking app",
   themeConfig: {
